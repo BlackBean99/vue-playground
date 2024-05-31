9611b3f222ee6d275c780a1cb742292615d708ca
blackbean99 <ymecca730135@gmail.com>
2024-05-31 13:39:56 +0900

    [feat]: v-on:click

---
+++ vue-router1/src/components/About.vue
@@ -0,0 +1,21 @@
+<template lang="">
+    <div>
+        <button v-on:click="move">Home으로 이동</button>
+        <br/>
+        <a :href="'/'">Home으로</a>
+    </div>
+</template>
+<script>
+export default {
+    name: 'About',
+    methods: {
+        move(){
+            this.$router.push('/');
+            
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
